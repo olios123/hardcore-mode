604ec94b530c0ed4b98c3f171e3bb32a333776b3
olios123 <oliwier4444k@gmail.com>
2026-01-10 19:16:36 +0100

    fix

--- src/main/java/me/olios/hardcoremode/Commands/Unban.java
+++ src/main/java/me/olios/hardcoremode/Commands/Unban.java
@@ -2,9 +2,8 @@ package me.olios.hardcoremode.Commands;
 
 import me.olios.hardcoremode.Data;
 import me.olios.hardcoremode.Framework.CommandAction;
+import me.olios.hardcoremode.Main;
 import me.olios.hardcoremode.Managers.MessagesManager;
-import me.olios.hardcoremode.Managers.PermissionsManager;
-import net.kyori.adventure.platform.bukkit.BukkitComponentSerializer;
 import org.bukkit.BanEntry;
 import org.bukkit.BanList;
 import org.bukkit.Bukkit;
@@ -12,7 +11,6 @@ import org.bukkit.OfflinePlayer;
 import org.bukkit.ban.ProfileBanList;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.profile.PlayerProfile;
 
 public class Unban extends CommandAction {
@@ -30,20 +28,20 @@ public class Unban extends CommandAction {
         OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(args[0]);
 
         // Player not found
-        if ((offlinePlayer == null || !offlinePlayer.hasPlayedBefore()) && bannedPlayer == null)
+        if (offlinePlayer == null || !offlinePlayer.hasPlayedBefore())
         {
             MessagesManager.sendMessageSender(sender, Data.Message.CMD_PLAYER_NOT_FOUND);
             return;
         }
 
-        BanEntry<PlayerProfile> entry = banList.getBanEntry(bannedPlayer);
-
         // Not banned
-        if (bannedPlayer == null || entry == null)
+        if (bannedPlayer == null || banList.getBanEntry(bannedPlayer) == null)
         {
             MessagesManager.sendMessageSender(sender, Data.Message.CMD_NOT_BANNED);
             return;
         }
+
+        BanEntry<PlayerProfile> entry = banList.getBanEntry(bannedPlayer);
 
         // Unban player
         banList.pardon(entry.getBanTarget());
@@ -53,7 +51,7 @@ public class Unban extends CommandAction {
     private static PlayerProfile getBannedPlayer(String name)
     {
         return banList.getEntries().stream()
-                .filter(entry -> name.equalsIgnoreCase(entry.getBanTarget().getName()))
+                .filter(entry -> entry.equals(name))
                 .map(BanEntry::getBanTarget)
                 .findFirst()
                 .orElse(null);
